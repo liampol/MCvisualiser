28f77cada5ca415bcc73c10e3207cbbfb8c62a4d
liampol <liampolnz@gmail.com>
2020-06-20 14:26:20 +1200

    Simple test for making sure tkinter & git are working

---
+++ tkinter_test.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+def main():
+    window = tk.Tk()
+    label = tk.Label(window, text='This is our label.')
+    label.grid(row=0, column=0)
+    window.mainloop()
+    
+main()
