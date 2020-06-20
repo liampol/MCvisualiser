49ff92e1756e74e51d2e817d0bda8bc7692f1fa9
liampol <liampolnz@gmail.com>
2020-06-20 16:54:28 +1200

    Created main file, and the Gui class within. Only creates a blank window so far

--- project/tkinter_test.py
+++ project/tkinter_test.py
@@ -1,11 +1,19 @@
 """Simple test to make sure tkinter and git are working"""
 
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
 def main():
-    window = tk.Tk()
-    label = tk.Label(window, text='This is our label.')
+    master = Tk()
+    set_master_settings(master)
+    label = Label(master, text='This is our label.')
     label.grid(row=0, column=0)
-    window.mainloop()
+    master.mainloop()
+    
+def set_master_settings(master_window):
+    """Sets the arguments of the master window to the final settings"""
+    master_window.geometry("800x600")
+    master_window.maxsize(800, 600)
+    #master_window.resizable(False)    
     
 main()
